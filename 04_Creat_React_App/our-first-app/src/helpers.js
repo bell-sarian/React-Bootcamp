8430b680699db250c3dd766e061823e822d155e8
bellsarian <bellasarian@gmail.com>
2021-01-29 09:26:21 -0800

    Section 4

---
+++ 04_Creat_React_App/our-first-app/src/helpers.js
@@ -0,0 +1,14 @@
+function helpful() {
+    console.log("I DID A HELLPFUL THING")
+}
+
+function sort() {
+    console.log("ALL SORTED!")
+}
+
+function sing() {
+    console.log("LA LA LA")
+}
+
+export default helpful; // DEFAULT thing to be exported is helpful in whole file
+export {sort, sing};
